44238529a1323c95eb1b7b96e020fe8c7105e668
Brishbhan Singh <brishbhansinghraja@gmail.com>
2025-08-20 10:36:52 +0530

    university update files

--- src/app/sharda/page.tsx
+++ src/app/sharda/page.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 // import { useNavigate } from 'react-router-dom';
 // import { Helmet } from 'react-helmet';
+import { motion } from "framer-motion";
 import {
   FaBookOpen,
   FaUserTie,
