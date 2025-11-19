ad21f62d57afc2be0c180a9123d769d827f52031
Shivam Jha <jha24978@gmail.com>
2025-11-19 17:36:57 +0530

    fix some issue

--- src/models/MobileAppUser.js
+++ src/models/MobileAppUser.js
@@ -3,10 +3,10 @@ import { Phone } from "lucide-react/dist/cjs/lucide-react";
 
 const MobileAppUserSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    fullname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    Phone: { type: String, required: true },
-    pasword: { type: String, required: true },
+    phone: { type: String, required: true },
+    password: { type: String, required: true },
   },
   { timestamps: true }
 );
